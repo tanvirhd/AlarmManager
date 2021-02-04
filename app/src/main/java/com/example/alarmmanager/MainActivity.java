4d93ffcfd6f005bdc6b45138aabdf41e6291061f
Tanvir Hossain <tanvir.taagidtech@gmail.com>
2021-02-04 22:42:31 +0600

    batterry optimization permission permission added

--- app/src/main/java/com/example/alarmmanager/MainActivity.java
+++ app/src/main/java/com/example/alarmmanager/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,6 +22,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.alarmmanager.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 import static java.security.AccessController.getContext;
 
@@ -42,20 +45,19 @@ public class MainActivity extends AppCompatActivity {
         myAlarmManager = new MyAlarmManager(MainActivity.this);
         powerManager = (PowerManager)getSystemService(Context.POWER_SERVICE);
 
-        if(powerManager.isIgnoringBatteryOptimizations("com.example.alarmmanager")){
-            Toast.makeText(this, "Not Optimized", Toast.LENGTH_SHORT).show();
-        }else{
+        if(!powerManager.isIgnoringBatteryOptimizations("com.example.alarmmanager")){
             requestPermission();
-            Toast.makeText(this, "Optimized", Toast.LENGTH_SHORT).show();
         }
 
+        addAutoStartup();
+
         //this will open auto start screen where user can enable autorun permission for your app
-        String manufacturer = "xiaomi";
+        /*String manufacturer = "xiaomi";
         if(manufacturer.equalsIgnoreCase(android.os.Build.MANUFACTURER)) {
             Intent intent = new Intent();
             intent.setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity"));
             startActivity(intent);
-        }
+        }*/
 
         /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Settings.canDrawOverlays(getApplicationContext())) {
             RequestPermission();
@@ -106,6 +108,33 @@ public class MainActivity extends AppCompatActivity {
     void requestPermission(){
         Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getApplicationContext().getPackageName()));
         startActivityForResult(intent, ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_REQUEST_CODE);
+    }
+
+    private void addAutoStartup() {
+
+        try {
+            Intent intent = new Intent();
+            String manufacturer = android.os.Build.MANUFACTURER;
+            Toast.makeText(this, ""+"MANUFACTURER : "+manufacturer, Toast.LENGTH_SHORT).show();
+            if ("xiaomi".equalsIgnoreCase(manufacturer)) {
+                intent.setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity"));
+            } else if ("oppo".equalsIgnoreCase(manufacturer)) {
+                intent.setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity"));
+            } else if ("vivo".equalsIgnoreCase(manufacturer)) {
+                intent.setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"));
+            } else if ("Letv".equalsIgnoreCase(manufacturer)) {
+                intent.setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity"));
+            } else if ("Honor".equalsIgnoreCase(manufacturer)||"Huawei".equalsIgnoreCase(manufacturer)) {
+                intent.setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity"));
+            }
+
+            List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            if  (list.size() > 0) {
+                startActivity(intent);
+            }
+        } catch (Exception e) {
+            Log.e("exc" , String.valueOf(e));
+        }
     }
 
     @Override
